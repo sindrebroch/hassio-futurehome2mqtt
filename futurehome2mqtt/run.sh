66140be65557e365d1d7e282974e8587ba8ed0b0
Sindre Broch <sindrebroch@gmail.com>
2024-02-05 21:57:07 +0100

    Debug

--- futurehome2mqtt/run.sh
+++ futurehome2mqtt/run.sh
@@ -10,5 +10,12 @@ export DEBUG=$(bashio::config 'debug')
 export SELECTED_DEVICES=$(bashio::config 'selected_devices')
 export PYTHONUNBUFFERED=1
 
+echo $FIMPSERVER
+echo $FIMPUSERNAME
+echo $FIMPPORT
+echo $CLIENT_ID
+echo $DEBUG
+echo $SELECTED_DEVICES
+
 echo Starting Futurehome FIMP to Home Assistant
 python3 run.py serve
